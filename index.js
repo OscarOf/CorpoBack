b4c00f1a08a5d6709bb5ed6d55cdffc24a31d5a3
Óscar O <oscarforjuela@hotmail.com>
2023-05-11 18:34:08 -0500

    New port

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 
 
 const app = express();
-const PORT = process.env.PORT||3030;
+const PORT = process.env.PORT;
 
 const connectionOptions ={ useUnifiedTopology: true,
     useNewUrlParser: true, useFindAndModify: false};
