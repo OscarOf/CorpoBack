53fe014cba9adeb265a573b61237ffe452b965bf
Óscar O <oscarforjuela@hotmail.com>
2023-05-11 18:57:04 -0500

    New cors

--- index.js
+++ index.js
@@ -2,11 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 
-const corsOptions ={
-   origin:'*', 
-   credentials:true,            
-   optionSuccessStatus:200,
-}
+
 
 const app = express();
 const PORT = process.env.PORT;
@@ -15,13 +11,21 @@ const connectionOptions ={ useUnifiedTopology: true,
     useNewUrlParser: true, useFindAndModify: false};
 
 app.use(express.json());
-app.use(cors(corsOptions));
+app.use(cors);
 
 mongoose.connect("mongodb+srv://fundacionsemillac3:1235@fundacionsemilladonacio.j2cjyrm.mongodb.net/?retryWrites=true&w=majority").then(() => console.log("Connected Successfully")).catch((err) => console.log(err));
 
 //mongoose.connect("mongodb://127.0.0.1:27017/FundacionSemillas").then(() => console.log("Connected Successfully")).catch((err) => console.log(err));
 
 app.use("/", require('./routes'));
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
 
 app.listen(PORT, ()=>{
     console.log("Mi puerto es "+ PORT);
