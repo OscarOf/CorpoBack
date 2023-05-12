d07a845627a463127d62c74bb4cb20824061d2dd
Óscar O <oscarforjuela@hotmail.com>
2023-05-12 00:04:16 -0500

    New port

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 
 const app = express()
-const PORT = process.env.PORT;
+const PORT = 3000;
 
 const connectionOptions ={ useUnifiedTopology: true,
     useNewUrlParser: true, useFindAndModify: false};
 
-app.use(cors({origin: "*", methods: ['GET', 'POST', 'PUT', 'DELETE']}));
-app.use(express.json());
 
+app.use(express.json());
+app.use(cors());
 
 mongoose.connect("mongodb+srv://fundacionsemillac3:1235@fundacionsemilladonacio.j2cjyrm.mongodb.net/?retryWrites=true&w=majority").then(() => console.log("Connected Successfully")).catch((err) => console.log(err));
 
